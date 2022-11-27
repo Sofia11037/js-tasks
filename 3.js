31196702a51b6599ae3d231b8271f8a8764f8913
Sofia11037 <113290148+Sofia11037@users.noreply.github.com>
2022-11-27 12:22:13 +0100

    Update 3.js

--- 3.js
+++ 3.js
@@ -84,15 +84,21 @@ for (let i = 0; i < numbers.length; i++) {
 // Добавить в каждый объект дополнительное поле usersAnswer со значением null.
 // Решение должно работать для массива любой длины.
 
-let questions = [{
-    question: "What's the currency of the USA?",
-    choices: ["US dollar", "Ruble", "Horses", "Gold"],
-    corAnswer: 0
-}, {
-    question: "Where was the American Declaration of Independence signed?",
-    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
-    corAnswer: 0
-}];
+    let questions = [{
+        question: "What's the currency of the USA?",
+        choices: ["US dollar", "Ruble", "Horses", "Gold"],
+        corAnswer: 0
+    }, {
+        question: "Where was the American Declaration of Independence signed?",
+        choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+        corAnswer: 0
+    }];
+
+    for (let i = -1; i < questions.length; i++) {
+        questions[i].usersAnswer = null;
+    }
+
+    console.log(questions);
 
 questions[0].usersAnswer = null;
 questions[1].usersAnswer = null;
